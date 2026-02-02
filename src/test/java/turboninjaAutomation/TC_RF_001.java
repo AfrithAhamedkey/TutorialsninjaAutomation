e9ba4553acd767f3b360c57348d1b870386aa12c
AfrithAhamedkey <afrithniyas@gmail.com>
2026-02-02 19:16:15 +0530

    testcase -1

---
+++ src/test/java/turboninjaAutomation/TC_RF_001.java
@@ -0,0 +1,41 @@
+package turboninjaAutomation;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class TC_RF_001 {
+	
+@Test
+	public void verifyRegisterAccountUsingMandatoryFields() {
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		
+		driver.get("https://tutorialsninja.com/demo");
+		driver.findElement(By.xpath("//span[text()='My Account']")).click();
+		driver.findElement(By.linkText("Register")).click();
+		driver.findElement(By.id("input-firstname")).sendKeys("Afrith");
+		driver.findElement(By.id("input-lastname")).sendKeys("Ahamed");
+		driver.findElement(By.id("input-email")).sendKeys(genarateEmail());
+		driver.findElement(By.id("input-telephone")).sendKeys("123456789");
+		driver.findElement(By.id("input-password")).sendKeys("1234");
+		driver.findElement(By.id("input-confirm")).sendKeys("1234");
+		driver.findElement(By.name("agree")).click();
+		driver.findElement(By.xpath("//input[@value='Continue']")).click();
+		
+		Assert.assertTrue(driver.findElement(By.xpath("//h1[text()='Your Account Has Been Created!']")).isDisplayed());
+	
+	}
+
+public String genarateEmail() {
+	String mail = "afrith"+System.currentTimeMillis()+"@gmail.com";
+
+	System.out.println(mail);
+	return mail;
+	}
+
+};

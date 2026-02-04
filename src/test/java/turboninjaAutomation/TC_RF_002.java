6c14d0f33aa9a68828cf1014ff69738ccc190141
AfrithAhamedkey <afrithniyas@gmail.com>
2026-02-04 09:10:37 +0530

    TC_RF_002 created

---
+++ src/test/java/turboninjaAutomation/TC_RF_002.java
@@ -0,0 +1,11 @@
+package turboninjaAutomation;
+
+import org.testng.annotations.Test;
+
+public class TC_RF_002 {
+	@Test
+	public void verifyEmailIsSentToTheRegisteredEmailAddressConfirmationForRegister() {
+		
+		System.out.println("Site is not sending mail, if do automation it will fail..");
+	}
+}
